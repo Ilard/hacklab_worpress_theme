608f2ac294004b601d21423bc1672e72e5b74c23
WOoOinux <contact@woinux.fr>
2019-12-30 18:37:16 +0100

    Header in progress

--- header.php
+++ header.php
@@ -3,8 +3,8 @@
     <head>
         <title><?php echo wp_title('|',true, 'right'); bloginfo('name'); ?></title>
         <meta charset="<?php bloginfo( 'charset' ); ?>">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
         <?php wp_head(); ?>
     </head>
     <body>
@@ -13,11 +13,15 @@
                 <div class="header-logo">
                     <a class="logo" href="#">Logo Hacklab</a>
                 </div>
-                <div class="header-menu-toggle">
-
-                </div>
                 <div class="header-menu">
-                    <?php wp_nav_menu(array('theme_location' => 'main_menu')); ?>
+                    <div class="header-menu-toggle">
+                        <div class="header-menu-toggle-content">
+                            <span></span>
+                        </div>
+                    </div>
+                    <div class="header-menu-content">
+                        <?php wp_nav_menu(array('theme_location' => 'main_menu')); ?>
+                    </div>
                 </div>
             </div>
         </header>
