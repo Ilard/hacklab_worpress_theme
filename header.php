0212d8be5bfd2448db4109594f4c69c5441a3b5c
WOoOinux <contact@woinux.fr>
2020-01-01 15:19:13 +0100

    Fix viewport problem on Chrome

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
     <head>
         <title><?php echo wp_title('|',true, 'right'); bloginfo('name'); ?></title>
         <meta charset="<?php bloginfo( 'charset' ); ?>">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
 
         <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,600,700,800&display=swap" rel="stylesheet">
 
